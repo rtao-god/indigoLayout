9ce2fd5d9119cfe7f58cab3c2c228f3f3e58978b
rtao-god <prostodimka519@gmail.com>
2022-11-14 22:40:11 +0300

    save1

--- lol.html/projects/indigoLayout/indigoLayout.js
+++ lol.html/projects/indigoLayout/indigoLayout.js
@@ -2,14 +2,20 @@ $(".h1").css({
     "margin" : "50px 0 0 0",
     "font-family" : "Arial",
     "font-size" : "25px",
-    "width" : "75%"
+    "width" : "75%",
+    "color" : "black"
 })
 
 $(".textP").css({
     "font-family" : "Arial",
     "font-size" : "17px",
     "margin" : "20px 0 20px 20px",
-    "width" : "75%"
+    "width" : "90%"
+})
+
+$(".tagPreExample").css({
+    "color" : "purple",
+    "font-size" : "15px"
 })
 
 
@@ -18,4 +24,19 @@ let date = new Date ().toLocaleDateString()
 
 let timeHeader = document.getElementById("timeHeader").innerHTML = date
 
-let 
+
+$("#viloletHrLeft").onmousemove = function(event) {
+    event = event || window.event
+
+    console.log(event)
+
+    
+}
+
+$("#blackHrRight").onmousemove = function(event) {
+
+}
+
+
+
+
