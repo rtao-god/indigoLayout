40f1f2df27a16e0dea87265927edf5d1a05cca08
rtao-god <prostodimka519@gmail.com>
2022-11-28 15:51:39 +0300

    topBarDrop done

--- lol.html/projects/indigoLayout/indigoLayout.js
+++ lol.html/projects/indigoLayout/indigoLayout.js
@@ -1,9 +1,13 @@
+let date = new Date ().toLocaleDateString()
+let timeHeader = document.getElementById("timeHeader").innerHTML = date
+
 $(".h1").css ({
     "margin" : "50px 0 0 0",
-    "font-family" : "PT Sans Narrow, arial, sans-serif;",
-    "font-size" : "21px",
+    "font-family" : "PT Sans Narrow, arial, sans-serif",
+    "font-size" : "19px",
     "width" : "70%",
-    "color" : "black"
+    "color" : "#111",
+    "font-height" : "27px;"
 }) 
 
 $(".textP").css ({
@@ -39,13 +43,13 @@ $("#textResult").css ({
 // }
 
 let screen = document.documentElement.clientWidth
-console.log(screen)
+console.log("screen: " + screen)
 
 document.querySelectorAll(".tagPExample").forEach((element) => {
 
     if ( screen < 771 ) { 
         element.style.marginLeft = "30px"
-        console.log(element)
+       
     } 
 })
 
@@ -53,51 +57,134 @@ document.querySelectorAll(".h1").forEach((element) => {
 
     if ( screen < 771 ) { 
         element.style.width = "100%"
-        console.log(element)
+       
     } 
 })
 
+
+
 // меняем width containerAll при сжатии экрана. Зачем? - margin containerAll влияет на левый и правый отступ всего сайта. 
-// При сжатии экрана margin уменьшается и в какой-то момент значение становится нулем и content сайта примыкает к границам экрана. Чтобы вернуть обратно отступы надо уменьшить width containerAll
-// margina, которое можно изменить при помощи width containerAll (при изменении width меняется margin). Но мы не можем
-// просто задавать всегда одно и то же значение width, так как на каждом сжатии экрана нужен разный width, но примерно - 213
+// При сжатии экрана margin уменьшается и в какой-то момент значение становится нулем и content сайта примыкает к границам экрана. 
+// Чтобы вернуть обратно отступы надо уменьшить width containerAll примерно на 50px
+
 
 let containerAll = document.getElementById("containerAll")
-console.log(containerAll)
 
+let marginLeft = parseInt(getComputedStyle(containerAll, null).marginLeft)
 
-let getMargin = window.getComputedStyle(containerAll).margin // получаем все значения margin, но нам нужно только второе значение
-console.log("margin: " + getMargin)
+console.log("marginLeft: " + marginLeft)
 
-let secondValueMargin = getMargin.split("px") // получаем массив с нужным нам значением margin без px (нужен second)
-console.log("marginSecond: " + secondValueMargin)
-
-
-let gethWidth = window.getComputedStyle(containerAll).width // получаем значение width + px
+let gethWidth = containerAll.offsetWidth 
 console.log("width: " + gethWidth)
 
-let width_Without_Px = gethWidth.split("px") // get массив без px
-console.log("widthSecond: " + width_Without_Px)
 
+if (marginLeft < 57 && screen > 1060 ) {    // 57 - примерно такой margin находится слишком близко к краю экрана. 1060 - примерно на таком разрешении экрана 213 не хватает, так что функция перестает работать 
+    
+    
+    let normalWidth = gethWidth - 50      // если условие выше вернет true, то это значит то, что width сейчас слишком большой для данного разрешения экрана. Примерно на 50 его надо уменьшить для получения нормального width
+    console.log("normalWidth: " + normalWidth)
 
-if (secondValueMargin[1] < 57 ) {    // 57 - примерно такой margin находится слишком близко к краю экрана
-
-    let normalWidth = width_Without_Px[0] - 213      // если условие выше вернет true, то это значит то, что width сейчас слишком большой для данного разрешения экрана. Примерно на 213 его надо уменьшить для получения нормального width
-                                                    
-        console.log(normalWidth)
-
-
-        let changeMargin = containerAll.style.margin = "0 auto"
-
-        console.log(changeMargin)
+    let changeWidth = containerAll.style.width = normalWidth + "px"                                        
+    console.log("modified width: " + changeWidth)
         
 
-        let changeWidth = containerAll.style.width = normalWidth + "px"    // меняем ширину на нормальную. Не забываем добавить на конце "px"
+    let marginLeft = parseInt(getComputedStyle(containerAll, null).marginLeft)
+    console.log("marginLeft: " + marginLeft)
 
+    let changeMarginRight = containerAll.style.marginRight = marginLeft + "px" 
+    let changeMarginLeft = containerAll.style.marginLeft = marginLeft + "px" 
 
+}
+
+function topBarDropFunction () {
+        
+    $("#dropTopBar").css ({
+        "height" : "100%",
+        "width" : "100%",
+        "float" : "none",
+        "margin-left" : "0",    
+    })
+
+    $("#topBar").css ({
+        "align-items" : "none",
+        "display" : "block",
+        "width" : "100%",
+        "height" : "100%",
+    })
+
+    $("#box2Drop a").css ({
+        "flex-wrap" : "wrap",
+        "align-content" : "center",
+        "justify-content" :  "flex-start",
+        "padding-left" : "15px",
+        "padding" : "10px",
+        "display" : "flex",
+        "height" : "30px",
+        "border-bottom-width" : "1px",
+        "background" : "url(photo/rightarrow.png) 98% 48% no-repeat",
+    })
+
+    $("#box1").css ({
+        "width" : "95%",
+    })
+
+    if(screen < 607) {
+        $("#box1").css ({
+            "width" : "94%",
+        })
+    }
+
+    if(screen < 501) {
+        $("#box1").css ({
+            "width" : "92%",
+        })
+    }
+
+    if(screen < 405) {
+        $("#box1").css ({
+            "width" : "91%",
+        })
+    }
+
+    if(screen < 405) {
+        $("#dropTopBar").css({
+            "margin-left" : "25%"
+        })
+    }
+
+    if(screen < 501) {
+        $("#box2Drop a").css ({
+            "background" : "url(photo/rightarrow.png) 95% 48% no-repeat"
+        })
+    
+        $("#dropTopBar").css ({
+            "margin-left" : "1%"
+        })
+    }
     
 }
 
+$("#arrowTopBar").click(function () {
+    $("#box2Drop").slideToggle(".active")
+
+    $("#box2Drop").css ({
+        "display" : "block"
+    })
+
+    $("#arrowTopBar").css({
+        "transform" : "rotate(0.5turn)"
+    })  
+})
+
+if(screen < 405) {
+    $("#dropTopBar").css({
+        "margin-left" : "25%"
+    })
+}
+
+$("#arrowTopBar").css({
+    "transform" : "rotate(1turn)"
+})
 
 
 
@@ -105,7 +192,8 @@ if (secondValueMargin[1] < 57 ) {    // 57 - примерно такой margin 
 
 
 
-let date = new Date ().toLocaleDateString()
-let timeHeader = document.getElementById("timeHeader").innerHTML = date
 
-let views = document.querySelector(".views")
+
+
+
+
