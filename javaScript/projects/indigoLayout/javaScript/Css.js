1d5e1583b8b8f6edd6f0fba7819d343f76581905
rtao-god <prostodimka519@gmail.com>
2022-12-23 07:54:17 +0300

    save

---
+++ javaScript/projects/indigoLayout/javaScript/Css.js
@@ -0,0 +1,25 @@
+$(".h1").css({
+    "margin": "50px 0 0 0",
+    "font-family": "PT Sans Narrow, arial, sans-serif",
+    "font-size": "19px",
+    "width": "70%",
+    "color": "#111",
+    "font-height": "27px;"
+})
+
+$(".textP").css({
+    "font-family": "Arial",
+    "font-size": "14px",
+    "margin": "20px 0 20px 0px",
+    "width": "90%"
+})
+
+$(".tagPExample").css({
+    "color": "purple",
+    "font-size": "20px",
+    "margin-left": "230px",
+})
+
+$("#textResult").css({
+    "margin-bottom": "150px"
+})
